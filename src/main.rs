f86b419bb901a004f68607526f5517f8866bf888
Nils Olsson <nilso@enosis.net>
2019-05-22 00:53:40 -0700

    Move clap config to yaml

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,51 @@
+#[macro_use]
 extern crate clap;
 
 use acm_rust::divisors::divisors;
 use acm_rust::prime_factors::prime_factors;
 use acm_rust::ArithmeticCongruenceMonoid as ACM;
-use clap::{App, AppSettings, Arg, SubCommand};
+use clap::App;
+
+fn parse_subcommand_arg(m: &clap::ArgMatches<'_>, arg: &str) -> u32 {
+    m.value_of(arg).unwrap().parse().unwrap()
+}
 
 fn main() {
-    let matches = App::new("acm_rust")
-        .about("ACM test program")
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .subcommand(
-            SubCommand::with_name("ACM")
-                .about("ACM subcommand")
-                .version("0.1")
-                .author("Nils Olsson <nilso@enosis.net>")
-                .arg(Arg::with_name("a").help("Component a").required(true))
-                .arg(Arg::with_name("b").help("Component b").required(true))
-                .setting(AppSettings::SubcommandRequiredElseHelp)
-                .subcommand(
-                    SubCommand::with_name("F")
-                        .arg(Arg::with_name("n").help("Integer").required(true)),
-                )
-                .subcommand(
-                    SubCommand::with_name("D")
-                        .arg(Arg::with_name("n").help("Integer").required(true)),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("F")
-                .about("Prime factorization")
-                .arg(Arg::with_name("n").help("Integer").required(true)),
-        )
-        .subcommand(
-            SubCommand::with_name("D")
-                .about("Divisors")
-                .arg(Arg::with_name("n").help("Integer").required(true)),
-        )
-        .get_matches();
-
+    let yaml = load_yaml!("main.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+    let verbose = matches.is_present("verbose");
     if let Some(matches) = matches.subcommand_matches("F") {
         let n = parse_subcommand_arg(&matches, "n");
-        println!("prime factors of {} : {:?}", n, prime_factors(n));
+        if verbose {
+            println!("integer prime power factorization of {} : {:?}", n, prime_factors(n));
+        } else {
+            println!("{:?}", prime_factors(n));
+        }
     } else if let Some(matches) = matches.subcommand_matches("D") {
         let n = parse_subcommand_arg(&matches, "n");
-        println!("divisors of {} : {:?}", n, divisors(n));
+        if verbose {
+            println!("integer divisors of {} : {:?}", n, divisors(n));
+        } else {
+            println!("{:?}", divisors(n));
+        }
     } else if let Some(matches) = matches.subcommand_matches("ACM") {
         let a = parse_subcommand_arg(&matches, "a");
         let b = parse_subcommand_arg(&matches, "b");
         let mut acm = ACM::new(a, b);
         if let Some(matches) = matches.subcommand_matches("F") {
             let n = parse_subcommand_arg(&matches, "n");
-            println!("{:?}", acm.factorize(n));
+            if verbose {
+                println!("ACM({},{}) prime power factorization of {} : {:?}", a, b, n, acm.factorize(n));
+            } else {
+                println!("{:?}", acm.factorize(n));
+            }
         } else if let Some(matches) = matches.subcommand_matches("D") {
             let n = parse_subcommand_arg(&matches, "n");
-            println!("{:?}", acm.divisors(n));
-            //println!("{:?} divisors of {} : {:?}", &acm, n, acm.divisors(n));
+            if verbose {
+                println!("ACM({},{}) divisors of {} : {:?}", a, b, n, acm.divisors(n));
+            } else {
+                println!("{:?}", acm.divisors(n));
+            }
         }
     }
-}
-
-fn parse_subcommand_arg(m: &clap::ArgMatches<'_>, arg: &str) -> u32 {
-    m.value_of(arg).unwrap().parse().unwrap()
 }
